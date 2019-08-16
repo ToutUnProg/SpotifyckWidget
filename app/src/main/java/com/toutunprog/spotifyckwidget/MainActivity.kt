87b0f5ad62c88f1bed0085c26e073f301655e255
ToutUnProg <tout.un.programme.yt@gmail.com>
2019-08-16 21:22:53 +0200

    Add spotify libs and communication between apps

--- app/src/main/java/com/toutunprog/spotifyckwidget/MainActivity.kt
+++ app/src/main/java/com/toutunprog/spotifyckwidget/MainActivity.kt
@@ -1,12 +1,62 @@
 package com.toutunprog.spotifyckwidget
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector
+import com.spotify.android.appremote.api.SpotifyAppRemote
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val CLIENT_ID = "48bfa25626844137bcbac9adc96143a4"
+    private val REDIRECT_URI = "com.yourdomain.yourapp://callback"
+    private var mSpotifyAppRemote: SpotifyAppRemote? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val connectionParams = ConnectionParams.Builder(CLIENT_ID)
+            .setRedirectUri(REDIRECT_URI)
+            .showAuthView(true)
+            .build()
+
+        SpotifyAppRemote.connect(this, connectionParams,
+            object : Connector.ConnectionListener {
+
+                override fun onConnected(spotifyAppRemote: SpotifyAppRemote) {
+                    mSpotifyAppRemote = spotifyAppRemote
+                    Log.d("MainActivity", "Connected! Yay!")
+
+                    // Now you can start interacting with App Remote
+                    connected()
+
+                }
+
+                override fun onFailure(throwable: Throwable) {
+                    Log.e("MyActivity", throwable.message, throwable)
+
+                    // Something went wrong when attempting to connect! Handle errors here
+                }
+            })
+    }
+
+    private fun connected() {
+        mSpotifyAppRemote?.playerApi?.subscribeToPlayerState()?.setEventCallback { playerState ->
+            val track = playerState.track
+            if (track != null) {
+                Log.d("MainActivity", track.name + " by " + track.artist.name)
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        SpotifyAppRemote.disconnect(mSpotifyAppRemote);
     }
 }
